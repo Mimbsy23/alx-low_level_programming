d25b01d39c2406eacef6d0798b072a227fe6ece0
Mimbsy23 <elviscobright@gmail.com>
2023-08-16 09:09:46 -0700

    100

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 
+
 #ifndef main_h
 #define main_h
 
@@ -14,5 +15,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
