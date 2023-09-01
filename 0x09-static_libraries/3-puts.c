f8d4eff243e75ff5bcb056037bf017927875cc7b
Mimbsy23 <elviscobright@gmail.com>
2023-09-01 11:06:02 -0700

    C - Static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str puts numbers i n a puzzle)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
