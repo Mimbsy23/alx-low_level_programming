d3165365926305e6ad01388027d87e9477dc122a
Mimbsy23 <elviscobright@gmail.com>
2023-09-13 10:39:09 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,50 +1,48 @@
-#include "function_pointers.h"
-	#include <stdlib.h>
-	#include <stdio.h>
-	#include "3-calc.h"
-	/**
-	 * main - Prints the result of simple operations.
-	 * @argc: The number of arguments supplied to the program.
-	 * @argv: An array of pointers to the arguments.
-	 *
-	 * Return: Always 0.
-	 */
-	int main(int __attribute__((__unused__)) argc, char *argv[])
+#include "3-calc.h"
+#include <string.h>
+
+/**
+ * main - function starts here.
+ * @argc: number of arguments in array
+ * @argv: array of arguments.
+ *
+ * Return: 0 if successful. 1 otherwise.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	/* Checks number of arguments */
+	if (argc < 4 || argc > 4)
 	{
-		int num1, num2;
-		char *op;
-
-
-		if (argc != 4)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-
-		num1 = atoi(argv[1]);
-		op = argv[2];
-		num2 = atoi(argv[3]);
-
-
-		if (get_op_func(op) == NULL || op[1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-
-
-		if ((*op == '/' && num2 == 0) ||
-		    (*op == '%' && num2 == 0))
-		{
-			printf("Error\n");
-			exit(100);
-		}
-
-
-		printf("%d\n", get_op_func(op)(num1, num2));
-
-
-		return (0);
+		printf("Error\n");
+		return (98);
 	}
 
+	/* Checks operator validity */
+	if ((*argv[2] != '+' && *argv[2] != '-'
+	&& *argv[2] != '*' && *argv[2] != '/'
+	&& *argv[2] != '%') || (strlen(argv[2]) > 1))
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	/* Check invalid division by 0 operation */
+	if ((*argv[2] == '/' || *argv[2] == '%')
+	&& *argv[3] == '0')
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	/* Store arguments in int variables */
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	/* Call function to check operator and add */
+	(*get_op_func(argv[2]))(i, j);
+
+	return (0);
+}
